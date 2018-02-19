0b7a7321ff18ee47b50830bca568ce457c21a032
Teodorek <36641881+Teodorek@users.noreply.github.com>
2018-02-19 18:55:25 +0100

    wrxutka
    52162

---
+++ buzi/src/buzi/Buzi.java
@@ -0,0 +1,44 @@
+
+package buzi;
+
+
+public class Buzi {
+
+    
+    public static void main(String[] args) {
+        Pracownicy Pierwszy = new Pracownicy("Marta", "Teodorowicz");
+        
+        Pracownicy Drugi = new Pracownicy();
+        
+        System.out.println(Pierwszy.imie + " " + Pierwszy.nazwisko + " ID = " + Pierwszy.ID);
+        System.out.println(Drugi.nazwisko + " ID = " + Drugi.ID);
+        
+        
+    }
+    
+}
+class Pracownicy
+{
+String imie;
+String nazwisko;
+int ID = 0;
+static int nastepnyID = 0;
+    Pracownicy()
+    {
+        
+        nastepnyID++;
+        ID = nastepnyID;
+    }
+    Pracownicy(String imie, String nazwisko)
+    {
+        this.imie = imie;
+        this.nazwisko= nazwisko;
+        nastepnyID++;
+        ID = nastepnyID;
+        
+    }
+    
+    void atakuj(){
+        System.out.println("ghraaaa");
+    }
+}
